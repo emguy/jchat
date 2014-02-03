086f708b0ba464b9a0699fae1124a3164b422797
emguy <emguy.yuzhang@gmail.com>
2014-02-03 11:21:57 -0500

    some fix

--- ClientConsole.java
+++ ClientConsole.java
@@ -2,77 +2,67 @@ import java.io.*;
 import client.*;
 import common.*;
 
-public class ClientConsole implements ChatIF{
+public class ClientConsole implements ChatIF
+{
 
 	final public static int DEFAULT_PORT = 5555;
 	ChatClient client;
 
-	public ClientConsole(String host, int port){
-		try{
+	public ClientConsole(String host, int port)
+    {
+		try
+        {
 			client= new ChatClient(host, port, this);
 		} 
-		catch(IOException exception){
+		catch(IOException exception)
+        {
 			this.display("Error: Can't setup connection!" + " Terminating client.");
 			System.exit(1);
 		}
 	}
 
-	public void accept(){
-		try{
+	public void accept()
+    {
+		try
+        {
 			BufferedReader fromConsole = new BufferedReader(new InputStreamReader(System.in));
 			String message;
 
-			while (true){
+			while (true)
+            {
 				message = fromConsole.readLine();
 				while(message.equals(""))
 					message = fromConsole.readLine();
 				client.handleMessageFromClientUI(message);
 			}
 		}
-		catch(Exception e){
+		catch(Exception e)
+        {
 			this.display("Unexpected error while reading from console!");
 		}
 	}
 
-	public void display(String message){
+	public void display(String message)
+    {
 		System.out.println(">"+ message);
 		System.out.print("$ ");
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args)
+    {
 		String host = "";
 		int port = 0;
-		try{
+		try
+        {
 			host = args[0];
 			port = Integer.parseInt(args[1]);
 		}
-		catch(ArrayIndexOutOfBoundsException e){
+		catch(ArrayIndexOutOfBoundsException e)
+        {
 			host = "localhost";
 			port = DEFAULT_PORT;
 		}
 		ClientConsole chat= new ClientConsole(host, port);
 		chat.accept();
 	}
-//	public static void main(String[] args){
-//		String host = "";
-//		String loginID = "";
-//		int port = 0;
-//		try{
-//			loginID = args[0];
-//		}
-//		catch(ArrayIndexOutOfBoundsException ex){
-//			System.out.println("<System> LoginID is required.");
-//			System.exit(1);
-//		}
-//		try{
-//			host = args[1];
-//			port = Integer.parseInt(args[2]);
-//		}
-//		catch(ArrayIndexOutOfBoundsException e){
-//			host = "localhost";
-//			port = DEFAULT_PORT;
-//		}
-//		ClientConsole chat= new ClientConsole(host, port, loginID);
-//		chat.accept();
-//	}
 }
--- ServerConsole.java
+++ ServerConsole.java
@@ -2,50 +2,62 @@ import java.io.*;
 import common.*;
 import server.*;
 
-public class ServerConsole implements ChatIF {
+public class ServerConsole implements ChatIF 
+{
 
 	final public static int DEFAULT_PORT = 5555;
 	ChatServer server;
 
-	public ServerConsole(int port){
-		try{
+	public ServerConsole(int port)
+    {
+		try
+        {
 			server = new ChatServer(port, this);
 			server.listen();
 		}
-		catch(IOException exception){
+		catch(IOException exception)
+        {
 			this.display("ERROR - Could not listen for clients!");
 			System.exit(1);
 		}
 	}
 
-	public void accept(){
-		try{
+	public void accept()
+    {
+		try
+        {
 			BufferedReader fromConsole = new BufferedReader(new InputStreamReader(System.in));
 			String message;
 
-			while (true){
+			while (true)
+            {
 				message = fromConsole.readLine();
 				while(message.equals(""))
 					message = fromConsole.readLine();
 				server.handleMessageFromServerUI(message);
 			}
 		}
-		catch (Exception ex){
+		catch (Exception ex)
+        {
 			this.display("Unexpected error while reading from console!");
 		}
 	}
 
-	public void display(String message){
+	public void display(String message)
+    {
 		System.out.println("> " + message);
 		System.out.print("$ ");
 	}
 
-	public static void main(String[] args){
+	public static void main(String[] args)
+    {
 		int port = 0;
-		try{
+		try
+        {
 			port = Integer.parseInt(args[0]);
 		}
-		catch(Throwable t){
+		catch(Throwable t)
+        {
 			port = DEFAULT_PORT;
 		}
 		ServerConsole sc = new ServerConsole(port);
